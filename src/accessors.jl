8b28b27412671a0566503c05a9d443980890d8fe
antimon2 <antimon2.me@gmail.com>
2019-01-30 00:19:32 +0900

    Use `QREF.qref` instead of `_qref`

--- src/accessors.jl
+++ src/accessors.jl
@@ -9,53 +9,73 @@ import Dates:
     days
 
 function year(qdt::QDate)
-    cqdate = _qref(qdt)
-    Int(cqdate[2])
+    # cqdate = _qref(qdt)
+    # Int(cqdate[2])
+    qdinfo = QREF.qref(date2jdn(qdt))
+    qdinfo.y
 end
 
 function month(qdt::QDate)
-    cqdate = _qref(qdt)
-    Int(cqdate[4])
+    # cqdate = _qref(qdt)
+    # Int(cqdate[4])
+    qdinfo = QREF.qref(date2jdn(qdt))
+    qdinfo.m
 end
 
 function day(qdt::QDate)
-    cqdate = _qref(qdt)
-    Int(cqdate[5])
+    # cqdate = _qref(qdt)
+    # Int(cqdate[5])
+    qdinfo = QREF.qref(date2jdn(qdt))
+    qdinfo.md
 end
 
 function yearmonth(qdt::QDate)
-    cqdate = _qref(qdt)
-    (Int(cqdate[2]), Int(cqdate[4]))
+    # cqdate = _qref(qdt)
+    # (Int(cqdate[2]), Int(cqdate[4]))
+    qdinfo = QREF.qref(date2jdn(qdt))
+    (qdinfo.y, qdinfo.m)
 end
 
 function monthday(qdt::QDate)
-    cqdate = _qref(qdt)
-    (Int(cqdate[4]), Int(cqdate[5]))
+    # cqdate = _qref(qdt)
+    # (Int(cqdate[4]), Int(cqdate[5]))
+    qdinfo = QREF.qref(date2jdn(qdt))
+    (qdinfo.m, qdinfo.md)
 end
 
 function yearmonthday(qdt::QDate)
-    cqdate = _qref(qdt)
-    (Int(cqdate[2]), Int(cqdate[4]), Int(cqdate[5]))
+    # cqdate = _qref(qdt)
+    # (Int(cqdate[2]), Int(cqdate[4]), Int(cqdate[5]))
+    qdinfo = QREF.qref(date2jdn(qdt))
+    (qdinfo.y, qdinfo.m, qdinfo.md)
 end
 
 function isleapmonth(qdt::QDate)
-    cqdate = _qref(qdt)
-    cqdate[7] != 0
+    # cqdate = _qref(qdt)
+    # cqdate[7] != 0
+    qdinfo = QREF.qref(date2jdn(qdt))
+    qdinfo.leap
 end
 
 function yearmonthleap(qdt::QDate)
-    cqdate = _qref(qdt)
-    (Int(cqdate[2]), Int(cqdate[4]), cqdate[7] != 0)
+    # cqdate = _qref(qdt)
+    # (Int(cqdate[2]), Int(cqdate[4]), cqdate[7] != 0)
+    qdinfo = QREF.qref(date2jdn(qdt))
+    (qdinfo.y, qdinfo.m, qdinfo.leap)
 end
 
 function monthleapday(qdt::QDate)
-    cqdate = _qref(qdt)
-    (Int(cqdate[4]), cqdate[7] != 0, Int(cqdate[5]))
+    # cqdate = _qref(qdt)
+    # (Int(cqdate[4]), cqdate[7] != 0, Int(cqdate[5]))
+    qdinfo = QREF.qref(date2jdn(qdt))
+    (qdinfo.m, qdinfo.leap, qdinfo.md)
 end
 
 function yearmonthleapday(qdt::QDate)
-    cqdate = _qref(qdt)
-    (Int(cqdate[2]), Int(cqdate[4]), cqdate[7] != 0, Int(cqdate[5]))
+    # cqdate = _qref(qdt)
+    # (Int(cqdate[2]), Int(cqdate[4]), cqdate[7] != 0, Int(cqdate[5]))
+    qdinfo = QREF.qref(date2jdn(qdt))
+    (qdinfo.y, qdinfo.m, qdinfo.leap, qdinfo.md)
 end
 
 @inline days(qdt::QDate) = value(qdt)
--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -1,44 +1,56 @@
 # arithmetic.jl
 
-function Base.:+(qdt::QDate,y::Year)
-    oy,om,ol,d = yearmonthleapday(qdt)
-    ny = oy + value(y)
-    vd = !ol || _rqref(Cint[FIRST_VALUE+DAYS_OFFSET,ny,0,om,1,0,ol]) > 0
-    nm = vd ? om : om + ol
-    nl = ol && vd
+function Base.:+(qdt::QDate, y::Year)
+    # oy,om,ol,d = yearmonthleapday(qdt)
+    qdinfo = QREF.qref(date2jdn(qdt))
+    # ny = oy + value(y)
+    ny = qdinfo.y + value(y)
+    # vd = !ol || _rqref(Cint[FIRST_VALUE+DAYS_OFFSET,ny,0,om,1,0,ol]) > 0
+    vd = !qdinfo.leap || QREF.rqref(ny, qdinfo.m, qdinfo.leap, 1).j > 0
+    # nm = vd ? om : om + ol
+    nm = vd ? qdinfo.m : qdinfo.m + qdinfo.leap
+    nl = qdinfo.leap && vd
     ld = daysinmonth(ny, nm, nl)
-    return QDate(ny, nm, nl, d <= ld ? d : ld)
+    return QDate(ny, nm, nl, qdinfo.md <= ld ? qdinfo.md : ld)
 end
-function Base.:-(qdt::QDate,y::Year)
-    oy,om,ol,d = yearmonthleapday(qdt)
-    ny = oy - value(y)
-    vd = !ol || _rqref(Cint[FIRST_VALUE+DAYS_OFFSET,ny,0,om,1,0,ol]) > 0
-    nm = vd ? om : om + ol
-    nl = ol && vd
+function Base.:-(qdt::QDate, y::Year)
+    # oy,om,ol,d = yearmonthleapday(qdt)
+    qdinfo = QREF.qref(date2jdn(qdt))
+    # ny = oy - value(y)
+    ny = qdinfo.y - value(y)
+    # vd = !ol || _rqref(Cint[FIRST_VALUE+DAYS_OFFSET,ny,0,om,1,0,ol]) > 0
+    vd = !qdinfo.leap || QREF.rqref(ny, qdinfo.m, qdinfo.leap, 1).j > 0
+    # nm = vd ? om : om + ol
+    nm = vd ? qdinfo.m : qdinfo.m + qdinfo.leap
+    nl = qdinfo.leap && vd
     ld = daysinmonth(ny, nm, nl)
-    return QDate(ny, nm, nl, d <= ld ? d : ld)
+    return QDate(ny, nm, nl, qdinfo.md <= ld ? qdinfo.md : ld)
 end
 
 const _DAYS_IN_MONTH_F = 29.530589
 
-function Base.:+(qdt::QDate,m::Month)
-    oy,om,ol,d = yearmonthleapday(qdt)
+function Base.:+(qdt::QDate, m::Month)
+    # oy,om,ol,d = yearmonthleapday(qdt)
+    oqdinfo = QREF.qref(date2jdn(qdt))
     days = ceil(Int, value(m) * _DAYS_IN_MONTH_F)
-    ny,nm,nl,_ = yearmonthleapday(QDate(UTD(value(qdt)-d+1+days)))
-    ld = daysinmonth(ny, nm, nl)
-    return QDate(ny, nm, nl, d <= ld ? d : ld)
+    # ny,nm,nl,_ = yearmonthleapday(QDate(UTD(value(qdt)-d+1+days)))
+    nqdinfo = QREF.qref(oqdinfo.j - oqdinfo.md + 1 + days)
+    ld = daysinmonth(nqdinfo.y, nqdinfo.m, nqdinfo.leap)
+    return QDate(nqdinfo.y, nqdinfo.m, nqdinfo.leap, oqdinfo.md <= ld ? oqdinfo.md : ld)
 end
-function Base.:-(qdt::QDate,m::Month)
-    oy,om,ol,d = yearmonthleapday(qdt)
+function Base.:-(qdt::QDate, m::Month)
+    # oy,om,ol,d = yearmonthleapday(qdt)
+    oqdinfo = QREF.qref(date2jdn(qdt))
     days = floor(Int, value(m) * _DAYS_IN_MONTH_F)
-    ny,nm,nl,_ = yearmonthleapday(QDate(UTD(value(qdt)-d+2-days)))
-    ld = daysinmonth(ny, nm, nl)
-    return QDate(ny, nm, nl, d <= ld ? d : ld)
+    # ny,nm,nl,_ = yearmonthleapday(QDate(UTD(value(qdt)-d+2-days)))
+    nqdinfo = QREF.qref(oqdinfo.j - oqdinfo.md + 2 - days)
+    ld = daysinmonth(nqdinfo.y, nqdinfo.m, nqdinfo.leap)
+    return QDate(nqdinfo.y, nqdinfo.m, nqdinfo.leap, oqdinfo.md <= ld ? oqdinfo.md : ld)
 end
 
-function Base.:+(qdt::QDate,d::Day)
-    QDate(UTD(days(qdt)+value(d)))
+function Base.:+(qdt::QDate, d::Day)
+    QDate(UTD(days(qdt) + value(d)))
 end
-function Base.:-(qdt::QDate,d::Day)
-    QDate(UTD(days(qdt)-value(d)))
+function Base.:-(qdt::QDate, d::Day)
+    QDate(UTD(days(qdt) - value(d)))
 end
